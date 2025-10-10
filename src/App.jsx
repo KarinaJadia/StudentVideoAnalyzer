bc8b49794a5138103fafca3f34f432928e6b4d02
Priya Jeyaprakash <priya.jeyaprakash@uconn.edu>
2025-10-10 14:20:58 -0400

    Fixed formating

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FileExplorer from './FileExplorer';
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <FileExplorer />
+      <Home />
     </>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,30 +7,41 @@ const Home = () => {
   return (
     <div id="home-page">
 
-        <h1 id="home-title">Welcome to Husky Lecture Log</h1>
+        <h1 id="home-title">Husky Lecture Log</h1>
 
-        <Navbar bg="primary" data-bs-theme="dark">
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="custom-navbar">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#videos">About HLL</Nav.Link>
-                    <Nav.Link href="#upload">HLL History</Nav.Link>
-                    <Nav.Link href="#about">Login</Nav.Link>
-                    <Nav.Link href="#about">Admin</Nav.Link>
-                </Nav>
+                
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#videos">About HLL</Nav.Link>
+                        <Nav.Link href="#upload">HLL History</Nav.Link>
+                        <Nav.Link href="#about">Login</Nav.Link>
+                        <Nav.Link href="#about">Admin</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
+
+        <h1>  </h1>
+        <h1>  </h1>
     
 
         <section id="search">
-            <h2>Learn from a new video!</h2>
+            <text id="welcome">Learn from a new video!</text>
+            <h1>  </h1>
+
+            
             <input type="text" id="link-button" placeholder="Enter a video link!"></input>
+            <h1>  </h1>
 
             <form id="file-button">
-                <label htmlFor="file-upload">Upload a file!</label>
-                <input type="file" id="file-upload" />
+                <input type="file" hidden id ="file-upload" />
+                <label for="file-upload" class="custom-file-upload">Upload a video File!</label>
             </form>
+
+            <h1>  </h1>
 
             <input type="text" id="cta-button" placeholder="Enter your question?"></input>    
         </section>
