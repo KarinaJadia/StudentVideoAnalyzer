cf3a689d3b4d5e8df79f2e1bfbd194a046567813
Priya Jeyaprakash <priya.jeyaprakash@uconn.edu>
2025-10-10 00:30:34 -0400

    Adding files for basic home page, will style more

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FileExplorer from './FileExplorer';
+import Home from './Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <FileExplorer />
+      <Home />
     </>
   )
 }
