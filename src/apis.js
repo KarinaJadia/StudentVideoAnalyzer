a0d5c95bb28a11eb3051e1a8b60f3f284897ad05
KarinaJadia <karina.jadia@gmail.com>
2025-10-30 14:05:51 -0400

    🗃️ stems to test

--- src/apis.js
+++ src/apis.js
@@ -21,6 +21,43 @@ export async function connectDB(password) {
   return request("/connect", "POST", { password });
 }
 
+export async function createUser(first_name, last_name, username, password_sha256) {
+  return request("/users", "POST", { first_name, last_name, username, password_sha256 });
+}
+
 export async function getUsers() {
   return request("/users", "GET");
+}
+
+export async function getUser(user_id) {
+  return request(`/users/${user_id}`, "GET");
+}
+
+export async function getUserChats(user_id) {
+  return request(`/users/${user_id}/chats`, "GET");
+}
+
+export async function createChat(user_id, chat_title, video_transcript = null) {
+  return request("/chats", "POST", { user_id, chat_title, video_transcript });
+}
+
+export async function getChat(chat_id) {
+  return request(`/chats/${chat_id}`, "GET");
+}
+
+export async function createChatLog(chat_id, role, content) {
+  return request("/chat_logs", "POST", { chat_id, role, content });
+}
+
+export async function getChatLogs(chat_id) {
+  return request(`/chat_logs/${chat_id}`, "GET");
+}
+
+export async function createPermission(user_id, upload_videos = false, save_transcript = false, access_admin_page = false
+) {
+  return request("/permissions", "POST", { user_id, upload_videos, save_transcript, access_admin_page });
+}
+
+export async function getPermission(user_id) {
+  return request(`/permissions/${user_id}`, "GET");
 }
